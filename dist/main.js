24330d12e26f08b2ee50836af0a0f40322e11a60
Snipey <stephenfreerking@gmail.com>
2015-10-06 00:11:33 -0700

    Prelim unitbuild

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ var transport = require('transport');
 //Lib
 var lca = require('logCreepActions');
 var numberWithCommas = require('numbersWithCommas');
+var util = require('util');
 //Logic
 var processHarvester = require('processHarvester');
 var processTransport = require('processTransport');
@@ -53,9 +54,40 @@ module.exports.loop = function () {
 
     console.log('Global Control Report - Level: ' + Game.gcl.level + ' - ' + numberWithCommas(Game.gcl.progress) + ' of ' + numberWithCommas(Game.gcl.progressTotal) + '.');
     //for(var p_room in p_rooms){
-        console.log('Room' + p_rooms + ' Energy: ' + numberWithCommas(p_rooms.energyAvailable) + ' of ' + numberWithCommas(p_rooms.energyCapacityAvailable));
-        console.log(' Energy: ' + numberWithCommas(p_rooms.energyAvailable) + ' of ' + numberWithCommas(p_rooms.energyCapacityAvailable));
-        console.log('all scripts completed ' + Game.time);
+        //console.log('Room' + p_rooms + ' Energy: ' + numberWithCommas(p_rooms.energyAvailable) + ' of ' + numberWithCommas(p_rooms.energyCapacityAvailable));
+        //console.log(' Energy: ' + numberWithCommas(p_rooms.energyAvailable) + ' of ' + numberWithCommas(p_rooms.energyCapacityAvailable));
+        //console.log('all scripts completed ' + Game.time);
         //console.log('totalEnergy: ' + numberWithCommas(totalEnergy(p_room)));
     //}
+/*    for (var creep in Memory.creeps) {
+        if (!Game.creeps[creep]) {
+            if (Memory.creeps[creep].safeToDelete) {
+                delete Memory.creeps[creep];
+            }
+            else {
+                Memory.creeps[creep].safeToDelete = true;
+            }
+        }
+    }
+    for (var room in Memory.rooms) {
+        if (!Game.rooms[room]) {
+            if (Memory.rooms[room].safeToDelete) {
+                delete Memory.rooms[room];
+            }
+            else {
+                Memory.rooms[room].safeToDelete = true;
+            }
+        }
+    }
+    for (var flag in Memory.flags) {
+        if (!Game.flags[flag]) {
+            if (Memory.flags[flag].safeToDelete) {
+                delete Memory.flags[flag];
+            }
+            else {
+                Memory.flags[flag].safeToDelete = true;
+            }
+        }
+    }*/
+
 };
